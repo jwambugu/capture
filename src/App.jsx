260cbb97d8b100597772c29028379b8a5759ed95
Joram Wambugu <artisan254@gmail.com>
2021-09-27 21:55:35 +0300

    Router
    Set up react router and update the navbar routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,30 @@ import GlobalStyles from "./components/GlobalStyles";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 import Navbar from "./components/Navbar";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <GlobalStyles />
             <Navbar />
-            <AboutUs />
+
+            <Switch>
+                <Route exact path="/">
+                    <AboutUs />
+                </Route>
+
+                <Route path="/our-work">
+                    <OurWork />
+                </Route>
+
+                <Route path="/contact-us">
+                    <ContactUs />
+                </Route>
+            </Switch>
         </div>
     );
 }

29d240b13eb9740325a58e37b953b30d7d3f62a6
Joram Wambugu <artisan254@gmail.com>
2021-09-27 21:29:01 +0300

    Create a nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import GlobalStyles from "./components/GlobalStyles";
 
 // Import pages
 import AboutUs from "./pages/AboutUs";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div className="App">
             <GlobalStyles />
+            <Navbar />
             <AboutUs />
         </div>
     );

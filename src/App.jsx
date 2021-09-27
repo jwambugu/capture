dff808c99bd8a981d54c304fa21acf491706efbd
Joram Wambugu <artisan254@gmail.com>
2021-09-27 14:28:33 +0300

    About us
    Create the section and add text data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+// Import pages
+import AboutUs from "./pages/AboutUs";
+
 function App() {
     return (
         <div className="App">
             <h1>Hello</h1>
+            <AboutUs />
         </div>
     );
 }

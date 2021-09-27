8d12842c355148974d4f902f6a2da1a119312669
Joram Wambugu <artisan254@gmail.com>
2021-09-27 16:38:22 +0300

    About Us
    Style services and faq sections

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,51 +1,86 @@
+// Styled Components
+import styled from "styled-components";
+import { About } from "../styles";
+
 function FaqSection() {
     return (
-        <div className="faq">
+        <Faq>
             <h2>
-                Any Question <span>FAQ</span>
+                Any Questions <span>FAQ</span>
             </h2>
             <div className="question">
-                <h4>How do I start?</h4>
+                <h4>How Do I Start?</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Consequuntur, nostrum!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Necessitatibus, neque.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
-                <h4>Daily schedule?</h4>
+                <h4>Daily Schedule</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Consequuntur, nostrum!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Necessitatibus, neque.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
-                <h4>Different payment methods?</h4>
+                <h4>Diferrent Payment Methods</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Consequuntur, nostrum!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Necessitatibus, neque.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
-                <h4>What products do you offer?</h4>
+                <h4>What Products do you offer.</h4>
                 <div className="answer">
-                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Consequuntur, nostrum!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Necessitatibus, neque.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     );
 }
+
+const Faq = styled(About)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -1,58 +1,62 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-* {
+
+*{
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    box-sizing: border-box
 }
 
-body {
-    background: #1b1b1b
+body{
+    background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
 }
 
-button {
+button{
     font-weight: bold;
-    font-size: 1.1rem;
+    font-size: 1.1.rem;
     cursor: pointer;
     padding: 1rem 2rem;
     border: 3px solid #23d997;
     background: transparent;
-    color: #ffffff;
+    color: white;
     transition: all 0.5s ease;
-    &:hover {
+    font-family: 'Inter', sans-serif;
+    &:hover{
         background-color: #23d997;
-        color: #ffffff;
+        color: white;
     }
 }
-
-h2 {
-    font-weight:lighter;
+    
+h2{
+    font-weight: lighter;
     font-size: 4rem;
 }
 
-h3 {
-    color: #ffffff;
+h3{
+    color: white;
 }
 
-h4 {
-    font-weight:bold;
+h4{
+    font-weight: bold;
+    font-size: 2rem;
 }
 
-p {
-    padding: 3rem, 0rem;
-    color: #cccccc;
-    font-size: 1.4rem;
-    line-height: 150%;
-}
-
-a {
+a{
     font-size: 1.1rem;
 }
 
-span {
-     font-weight:bold;
-    color: #23d997;
+span{
+    font-weight: bold;
+    color:#23d997;
+}
+
+p{
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
 }
 
 `;

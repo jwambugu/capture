d69e883bd2c25c2dd5d7d7f778b48af907d8eb23
Joram Wambugu <artisan254@gmail.com>
2021-09-28 16:33:01 +0300

    Animate about us page

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,36 +6,42 @@ import { About, Description, Hide, Image } from "../styles";
 
 // Animations
 import { motion } from "framer-motion";
+import { fade, photoAnimation, titleAnimation } from "../animations";
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <motion.div className="title">
+        <motion.div variants={titleAnimation} className="title">
           <Hide>
             <motion.h2>We work to make</motion.h2>
           </Hide>
 
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
 
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </Hide>
         </motion.div>
 
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
 
       <Image>
-        <img src={homeOne} alt="guy with a camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={homeOne}
+          alt="guy with a camera"
+        />
       </Image>
     </About>
   );

d315013c32719187bbebdb694fb7844c825ecd40
Joram Wambugu <artisan254@gmail.com>
2021-09-29 14:36:37 +0300

    Animate faq section

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,6 +1,9 @@
 // Styled Components
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
+
+import { AnimateSharedLayout } from "framer-motion";
 
 function FaqSection() {
   return (
@@ -8,50 +11,48 @@ function FaqSection() {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, neque.
-          </p>
-        </div>
-        <div className="faq-line" />
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, neque.
-          </p>
-        </div>
-        <div className="faq-line" />
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, neque.
-          </p>
-        </div>
-        <div className="faq-line" />
-      </div>
-      <div className="question">
-        <h4>What Products do you offer.</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, neque.
-          </p>
-        </div>
-        <div className="faq-line" />
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+              autem accusamus ex laboriosam porro, adipisci quam voluptatum
+              magnam placeat corporis.
+            </p>
+          </div>
+        </Toggle>
+
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error,
+              totam.
+            </p>
+          </div>
+        </Toggle>
+
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+
+        <Toggle title="What Products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 }
@@ -71,20 +72,20 @@ const Faq = styled(About)`
   .faq-line {
     background: #cccccc;
     height: 0.2rem;
-    margin: 2rem 0rem;
+    margin: 2rem 0;
     width: 100%;
   }
 
   .question {
-    padding: 3rem 0rem;
+    padding: 2rem 0;
     cursor: pointer;
   }
 
   .answer {
-    padding: 2rem 0rem;
+    padding: 2rem 0;
 
     p {
-      padding: 1rem 0rem;
+      padding: 1rem 0;
     }
   }
 `;
